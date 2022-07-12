d7b6363461b9bed30a847d581dc320fb319c1271
Qinzheng Sun <105312260+qinzheng-sun@users.noreply.github.com>
2022-07-12 16:37:45 -0400

    Update sketch.js

--- OneDrive_1_7-12-2022/07_twoParameters/sketch.js
+++ OneDrive_1_7-12-2022/07_twoParameters/sketch.js
@@ -1,7 +1,8 @@
 let dataServer;
-let pubKey = "YOUR PUBLISH KEY HERE";
-let subKey = "YOUR SUBSCRIBE KEY HERE";
-let secretKey = "YOUR SECRET KEY HERE";
+let pubKey = "pub-c-c69a8ba6-a9b0-46b7-88cd-f95a56118ac1";
+let subKey = "sub-c-e6326f63-94b2-46d8-8344-56399a3cee02";
+let secretKey = "sec-c-OTUwYTMwZDMtZTc0My00N2Q2LWJkZjMtOTYwNWVhZDIyOTk1";
+
 
 let channelName = "NFCs"; // we don't define this right away!
 
